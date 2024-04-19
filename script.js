08dffc9fa88cbc4a8a90f94d6e482e18edfc838b
sam-starikov <sam_starikov@rambler.ru>
2024-04-19 11:56:25 +0400

    fix bugs and refactoring

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 window.addEventListener('scroll', () => {
-	console.log(
-		document.querySelector('.abonnement__btn').offsetTop,
-		window.scrollY
-	)
 	if (window.scrollY > document.querySelector('.abonnement').offsetTop) {
 		document.querySelector('.btn').classList.add('none')
 	} else {
